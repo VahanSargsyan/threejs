1848162e55ab10c7e3928eead84de4d5cd801076
Vahan <vahansargsyan1310@gmail.com>
2017-09-23 10:57:49 +0400

    add moon and saturn ring

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const c = console;
-const dN =  degreeNominal = 0.10471666666666667 // degreeNominal = 6^
+const dN = 0.10471666666666667 //dN is degreeNominal = 6^
 
-const planetCreator = (r, dist, name) => {
+const planetCreator = (r, dist, name, speed) => {
     let link = '/threejs/img/' + name + '.jpg'
     const texture = new THREE.TextureLoader().load(link);
     texture.wrapS = THREE.RepeatWrapping;
@@ -10,13 +10,32 @@ const planetCreator = (r, dist, name) => {
     const planetGeo = new THREE.SphereGeometry(r, 36, 36);
     const planetMat = new THREE.MeshLambertMaterial({ map: texture });
     const planet = new THREE.Mesh(planetGeo, planetMat);
+    planet.name = name;
     planet.position.x = dist;
     planet.dist = planet.dist || dist;
-    planet.speed = 5 / (r / 4);
+    planet.speed = speed / 40;
     planet.xDirection = -1;
     planet.zDirection = -1;
     return planet;
 }
+const satelliteing = ( parent, satEl, distance, speed = 0) => {
+    satEl.position.x = parent.position.x + distance;
+    satEl.position.y = parent.position.y;
+    
+    satEl.animation = () => {
+        satEl.rotation.z += 0.003;
+        if (satEl.position.x >= distance + parent.position.x) satEl.xDirection = -1;
+        if (satEl.position.x <= -(distance + parent.position.x)) satEl.xDirection = 1; 
+        satEl.position.x = parent.position.x + distance;
+        satEl.position.z = parent.position.z;
+        
+        // satEl.position.x += (speed * satEl.xDirection);  
+        // el.position.z = (Math.sqrt( el.dist ** 2 - el.position.x ** 2 ) * el.xDirection);         
+   } 
+
+}
+
+
 window.onload = function() {
         //camera custome  control start
     document.onkeydown  = e =>{ //keyboard control
@@ -42,26 +61,43 @@ window.onload = function() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     const solarSystem = [
-        planetCreator(300, 0, 'sun'),         //sun
-        planetCreator(11.88, 600, 'mercury'),     //mercury
-        planetCreator(12.2, 800, 'venera'),      //venera
-        planetCreator(12.9, 1100, 'eart'),     //Eart
-        planetCreator(6.8, 1350, 'mars'),      //mars
-        planetCreator(142.8, 2000, 'jupiter'),    //jupiter
-        planetCreator(120.66, 2500, 'saturn'),   //saturn
-        planetCreator(51.12, 2900, 'uranus'),    //uranus
-        planetCreator(49.53, 3400, 'neptune'),    //neptune
-        planetCreator(2.1, 3500, 'pluto'),      //pluto
+        // planetCreator( radiusOfPlanet, distanceFromSun, textureName, speed )
+        planetCreator(300, 0, 'sun', 0),         //sun
+        planetCreator(11.88, 600, 'mercury', 47.87),     //mercury
+        planetCreator(12.2, 800, 'venera', 35),      //venera
+        planetCreator(12.9, 1100, 'eart', 30),     //Eart
+        planetCreator(6.8, 1350, 'mars', 24),      //mars
+        planetCreator(142.8, 2000, 'jupiter', 13),    //jupiter
+        planetCreator(120.66, 2500, 'saturn', 10),   //saturn
+        planetCreator(51.12, 2900, 'uranus', 7),    //uranus
+        planetCreator(49.53, 3400, 'neptune', 5.5),    //neptune
+        planetCreator(2.1, 3500, 'pluto', 3),      //pluto
+      
     ]
+    ////==================>satellites start
+    const satellites = [];
+
+    const moon =  planetCreator(2.5, 0, 'moon', 0 );
+    satelliteing(solarSystem[3], moon, 30);
+    satellites.push(moon);
+
+    const saturnRing = new THREE.Mesh(new THREE.RingGeometry(150, 230, 30), new THREE.MeshBasicMaterial({ color: 0xf4f4f4, side: THREE.DoubleSide }));
+    saturnRing.rotation.x = 15 * dN;
+    satelliteing(solarSystem[5], saturnRing, 0)
+    satellites.push(saturnRing);
+      ////==================>satellites End
+    
     //// add spaces 
     const spacesGeo = new THREE.SphereGeometry(50000, 36, 36);
     const spacesTexture = new THREE.TextureLoader().load('/threejs/img/stars.jpg');
     const spacesMaterial = new THREE.MeshPhongMaterial({ map: spacesTexture });
     const spaces = new THREE.Mesh(spacesGeo, spacesMaterial);
     spaces.material.side = THREE.BackSide;
-    scene.add(spaces);    
-    scene.add(...solarSystem);
-    
+
+    //experimental zone
+   
+   
+    // const atmosphere = new THREE.FrontSide(sphere);
     
     
    
@@ -74,54 +110,22 @@ window.onload = function() {
     const sunLight2 = new THREE.PointLight(0xffffff, 2, 3600, 2);
     sunLight2.position.set(0, -50, 0);
     scene.add(sunLight2);
-
-    //experimental zone/////////////////////////////////////////
-    // const texture = new THREE.TextureLoader().load('../img/eartTexture.jpg');
-    // texture.wrapS = THREE.RepeatWrapping;
-    // texture.wrapT = THREE.RepeatWrapping;
-    // texture.repeat.set( 1, 1 );
-    // const secondEartMaterial = new THREE.MeshBasicMaterial({ map: texture })
-    // const secondEartGeo = new THREE.SphereGeometry(50, 36, 36);
-    // const secondEart = new THREE.Mesh(secondEartGeo, secondEartMaterial);
-    // secondEart.position.set(0, 600, 0);
-    // scene.add( secondEart );
-
-
-
-    // const light2 = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-    // scene.add( light2 );
-// GUI setup start
-    // const sphereAnim = () => {
-    //      this.rotationX = 0,
-    //      this.rotationY = 0,
-    //      this.rotationZ = 0,
-    //      this.positionX = 0,
-    //      this.positionY = 0,
-    //      this.positionZ = 0
-    // }
-    // const anim = new sphereAnim();
-    // const gui = new dat.GUI();
-    // gui.add(anim, rotationX).min(-0.1).max(0.1).step(0.01);
-    // gui.add(anim, rotationY).min(-0.1).max(0.1).step(0.01);
-    // gui.add(anim, rotationZ).min(-0.1).max(0.1).step(0.01);
-    // gui.add(anim, positionX).min(-2).max(2).step(0.1);
-    // gui.add(anim, positionY).min(-2).max(2).step(0.1);
-    // gui.add(anim, positionZ).min(-2).max(2).step(0.1);
-// GUI setup end
+    scene.add(spaces);    
+    scene.add(...solarSystem);
+    scene.add(...satellites);
     const animate = function () {
         requestAnimationFrame( animate );
         solarSystem.map( el => {
              el.rotation.y += 0.01;
              if (el.position.x >= el.dist) el.xDirection = -1;
-             if (el.position.x <= -el.dist) el.xDirection = 1;
-             if (el.position.z >= el.dist) el.zDirection = -1;
-             if (el.position.z <= -el.dist) el.zDirection = 1;  
+             if (el.position.x <= -el.dist) el.xDirection = 1; 
              el.position.x += (el.speed * el.xDirection);  
-             el.position.z += (el.speed * el.zDirection);         
+             el.position.z = (Math.sqrt( el.dist ** 2 - el.position.x ** 2 ) * el.xDirection);         
+        })
+        satellites.map(el => {
+            el.animation();
         }) 
         renderer.render(scene, camera);
     };
     animate();
 }
-
-
